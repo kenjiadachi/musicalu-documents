c7f1a4453f9343f5ee54530cb84ce37dca8a6e3f
安達 健二 <kenji.183927@gmail.com>
2019-12-09 23:43:30 +0900

    2019:12:09-23:43:30

---
+++ docs/en.search.min.93ab46d76eedb6fae5f3189a18241d3505cf244a7fbcd2567b497fad2353ce48.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/musicalu-documents/flexsearch.min.js');loadScript('/musicalu-documents/en.search-data.min.f5bcef728652477117773be8b3514513d8a03c23e9ff3ca78a2d72d6a9127673.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
